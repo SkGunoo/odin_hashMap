ff9ea1d103bd4ae99389efb6479420f33a0e2549
Gunoo Kim <73500662+SkGunoo@users.noreply.github.com>
2025-06-12 12:16:36 +0100

    initial HashMap class set up

---
+++ hashmap.rb
@@ -0,0 +1,15 @@
+class HashMap
+
+  def initialize(capcacity, load_factor)
+    @capcacity = capcacity
+    @load_factor = load_factor
+  end
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord}
+    hash_code
+  end
+end
